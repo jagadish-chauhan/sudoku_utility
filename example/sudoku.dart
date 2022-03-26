f07a8b2eea97f9a4542ef515390ed63f1f6ff8f4
Jagadish Chauhan <myjagdish@yahoo.com>
2022-03-26 17:18:52 +0530

    Add Examples

---
+++ example/sudoku.dart
@@ -0,0 +1,25 @@
+import 'package:sudoku_utility/sudoku_utility.dart';
+
+void main() {
+  // Solve Example
+  var sudoku = <List<int?>>[
+    [2, 1, 7, 4, 3, 5, 9, null, 8],
+    [5, 8, 9, 0, 6, 2, 4, 3, null],
+    [3, 4, 6, 0, 7, 9, 2, 5, 1],
+    [1, 6, 2, 3, 0, 0, 5, 4, 0],
+    [9, 0, 5, 2, 0, 4, 7, 8, 6],
+    [4, 7, 8, 5, 9, 6, 1, 0, 0],
+    [8, 0, 3, 0, 4, 1, 6, 7, 2],
+    [0, 9, 4, 7, 2, 3, 8, 1, 5],
+    [7, 2, 1, 0, 5, 0, 0, 0, 4],
+  ];
+  var sudokuSolved = SudokuSolver.solve(sudoku);
+  SudokuUtilities.printSudoku(sudokuSolved);
+  print(''); // ignore: avoid_print
+
+  // Generate Example
+  var sudokuGenerator = SudokuGenerator(emptySquares: 54);
+  SudokuUtilities.printSudoku(sudokuGenerator.newSudoku);
+  print(''); // ignore: avoid_print
+  SudokuUtilities.printSudoku(sudokuGenerator.newSudokuSolved);
+}
